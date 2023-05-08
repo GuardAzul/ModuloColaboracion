9c2e3038925a4931ab84f1cebaf4bea1e3725fe5
Julián Acosta <julian.acostat@uqvirtual.edu.co>
2023-05-07 21:22:51 -0500

    Nueva vista 'test' y nuevos controladores dinamicos para 'proyecto'

---
+++ src/App/Controllers/dinamico/ProyectoController.java
@@ -0,0 +1,25 @@
+package App.Controllers.dinamico;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class ProyectoController implements Initializable {
+
+    @FXML private AnchorPane anchorPane;
+    @FXML private Label idProyecto;
+    @FXML private Label nombreProyecto;
+    @FXML private Label estadoProyecto;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
